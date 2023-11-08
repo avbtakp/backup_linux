9f43067c6edf2e0fd93c9e8bbeda51d0160f1433
avbtakp <150269294+avbtakp@users.noreply.github.com>
2023-11-08 10:17:51 -0600

    Update Permisos archivo ScriptBackup.sh

--- Permisos archivo ScriptBackup.sh
+++ Permisos archivo ScriptBackup.sh
@@ -1,3 +1,8 @@
 Debes de otorgar permisos al archivo ScriptBackup.sh una vez ya creado. ejemplo:
+Crear el archivo 
 
-chmod +x ScriptBackup.sh
+1. nano ScriptBackup.sh
+Este el cual está su Script y archivo para su descarga.
+
+2. chmod +x ScriptBackup.sh
+Comando Script para otorgar permisos al archivo creado posteriormente
